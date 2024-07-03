05cebd3822dbec1bc0926bf14c63c7ead3ded938
damien <damien.dutertre@limayrac.fr>
2024-07-03 21:38:40 +0200

    rajout

--- frontend/nom-du-projet/src/views/AdminMetroPlan.vue
+++ frontend/nom-du-projet/src/views/AdminMetroPlan.vue
@@ -1,35 +1,32 @@
 <template>
-  <div>
+  <div class="container">
     <AppNavbar />
-    
-      <h2 class="text-3xl font-semibold mb-8 text-center">Plan Transport de la Ville</h2>
-      <div ref="mapContainer" class="map-container relative overflow-hidden cursor-grab" @wheel="zoomMap"
-           @mousedown="startPanning" @mousemove="movePanning" @mouseup="endPanning" @mouseleave="endPanning">
-        <svg xmlns="http://www.w3.org/2000/svg" :style="{ transform: mapTransform }" :viewBox="viewBox">
-          <g v-for="(rue, rueIndex) in rues" :key="rueIndex">
-            <polyline :points="getPolylinePoints(rue.arrets, rueIndex)" :stroke="colors[rueIndex % colors.length]"
-                      stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" />
-            <g>
-              <circle v-for="(arret, index) in rue.arrets" :key="index"
-                      :cx="calculateAdjustedPosition(rueIndex, index, 'x')"
-                      :cy="calculateAdjustedPosition(rueIndex, index, 'y')" r="8" fill="white" stroke="black"
-                      stroke-width="2" @mouseover="showTooltip($event, arret.nom)" @mouseleave="hideTooltip" />
-              <text v-for="(arret, index) in rue.arrets" :key="`text-${index}`"
-                    :x="calculateAdjustedPosition(rueIndex, index, 'x')" :y="calculateAdjustedPosition(rueIndex, index, 'y')"
-                    dx="10" dy="-10" font-size="8" text-anchor="start" fill="gray">
-                {{ arret.nom.substring(0, 5) }}
-              </text>
-            </g>
+    <h2 class="text-3xl font-semibold mb-8 text-center">Plan Transport de la Ville</h2>
+    <div ref="mapContainer" :class="{'cursor-grab': !panning, 'cursor-grabbing': panning}" class="map-container relative" @wheel="zoomMap" @mousedown="startPanning" @mousemove="movePanning" @mouseup="endPanning" @mouseleave="endPanning">
+      <svg ref="svgElement" :viewBox="viewBox" width="100%" height="100%">
+        <g v-for="(rue, rueIndex) in rues" :key="rueIndex">
+          <polyline :points="getPolylinePoints(rue.arrets, rueIndex)" :stroke="colors[rueIndex % colors.length]" 
+                    stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" />
+          <g>
+            <circle v-for="(arret, index) in rue.arrets" :key="index"
+                    :cx="calculateAdjustedPosition(rueIndex, index, 'x')"
+                    :cy="calculateAdjustedPosition(rueIndex, index, 'y')" r="8" fill="white" stroke="black"
+                    stroke-width="2" @mouseover="showTooltip($event, arret.nom)" @mouseleave="hideTooltip" />
+            <text v-for="(arret, index) in rue.arrets" :key="`text-${index}`"
+                  :x="calculateAdjustedPosition(rueIndex, index, 'x')"
+                  :y="calculateAdjustedPosition(rueIndex, index, 'y')"
+                  dx="10" dy="-10" font-size="8" text-anchor="start" fill="gray">
+              {{ arret.nom.substring(0, 5) }}
+            </text>
           </g>
-        </svg>
-        <div v-if="tooltip.visible" :style="{ top: tooltip.top + 'px', left: tooltip.left + 'px' }" class="tooltip">
-          {{ tooltip.content }}
-        </div>
+        </g>
+      </svg>
+      <div v-if="tooltip.visible" :style="{ top: tooltip.top + 'px', left: tooltip.left + 'px' }" class="tooltip">
+        {{ tooltip.content }}
       </div>
-    
+    </div>
   </div>
 </template>
-
 <script>
 import axios from 'axios';
 import AppNavbar from '@/components/AppNavbar.vue';
@@ -41,7 +38,6 @@ export default {
   data() {
     return {
       rues: [],
-      mapTransform: 'scale(1)',
       panning: false,
       startX: 0,
       startY: 0,
@@ -54,11 +50,23 @@ export default {
         left: 0
       },
       colors: ['#FF6347', '#008080', '#FFD700', '#800080', '#4682B4', '#ADFF2F', '#FF69B4', '#CD5C5C', '#4B0082', '#FF4500'],
-      viewBox: '0 0 1000 700' // Adjust initial viewBox for a better fit
+      viewBox: '0 0 2000 2000',  // Initialize the viewBox to sensible default values
+      scaleFactor: 1, // Facteur de zoom
+      svgWidth: 2000,
+      svgHeight: 2000,
+      viewBoxX: 0,
+      viewBoxY: 0,
+      viewPortWidth: 2000,
+      viewPortHeight: 2000
     };
   },
   mounted() {
     this.fetchRuesEtArrets();
+    this.$nextTick(() => {
+      const rect = this.$refs.svgElement.getBoundingClientRect();
+      this.svgWidth = rect.width;
+      this.svgHeight = rect.height;
+    });
   },
   methods: {
     async fetchRuesEtArrets() {
@@ -102,7 +110,7 @@ export default {
       return Object.values(rues);
     },
     calculatePositionBase(coordinates, axis) {
-      const gridSize = 10000; // Base grid size adjusted for large plan
+      const gridSize = 40000; // Base grid size adjusted for larger plan (previously 20000)
       const offsetX = 0;
       const offsetY = 0;
 
@@ -129,23 +137,46 @@ export default {
     zoomMap(event) {
       event.preventDefault();
       const scaleAmount = 0.1;
-      const scaleFactor = event.deltaY < 0 ? 1 - scaleAmount : 1 + scaleAmount;
-      const [x, y, width, height] = this.viewBox.split(' ').map(Number);
-      const newWidth = width * scaleFactor;
-      const newHeight = height * scaleFactor;
+      const direction = event.deltaY > 0 ? -1 : 1; // +1 zoom in, -1 zoom out
 
-      this.viewBox = `${x} ${y} ${newWidth} ${newHeight}`;
+      // Calculate new scale factor with constraining it within 0.5 to 5 range
+      let newScale = this.scaleFactor + (direction * scaleAmount);
+      newScale = Math.max(0.1, Math.min(5, newScale)); // prevent too much zoom in or out
+
+      // Calculate the new dimensions of the viewBox
+      const newWidth = this.viewPortWidth * (this.scaleFactor / newScale);
+      const newHeight = this.viewPortHeight * (this.scaleFactor / newScale);
+
+      // Calculate new x, y positions to center the zoom at cursor location
+      const mouseXRatio = event.clientX / this.svgWidth;
+      const mouseYRatio = event.clientY / this.svgHeight;
+      this.viewBoxX += (this.viewPortWidth - newWidth) * mouseXRatio;
+      this.viewBoxY += (this.viewPortHeight - newHeight) * mouseYRatio;
+
+      this.viewPortWidth = newWidth;
+      this.viewPortHeight = newHeight;
+      this.scaleFactor = newScale;
+
+      // Update viewBox with the new dimensions
+      this.updateViewBox();
+    },
+    updateViewBox() {
+      this.viewBox = `${this.viewBoxX} ${this.viewBoxY} ${this.viewPortWidth} ${this.viewPortHeight}`;
     },
     startPanning(event) {
       this.panning = true;
-      this.startX = event.clientX - this.offsetX;
-      this.startY = event.clientY - this.offsetY;
+      this.startX = event.clientX;
+      this.startY = event.clientY;
     },
     movePanning(event) {
       if (!this.panning) return;
-      this.offsetX = event.clientX - this.startX;
-      this.offsetY = event.clientY - this.startY;
-      this.mapTransform = `translate(${this.offsetX}px, ${this.offsetY}px) scale(1)`;
+      const dx = (event.clientX - this.startX) * (this.viewPortWidth / this.svgWidth);
+      const dy = (event.clientY - this.startY) * (this.viewPortHeight / this.svgHeight);
+      this.startX = event.clientX;
+      this.startY = event.clientY;
+      this.viewBoxX -= dx;
+      this.viewBoxY -= dy;
+      this.updateViewBox();
     },
     endPanning() {
       this.panning = false;
@@ -153,18 +184,27 @@ export default {
     calculateAndSetViewBox() {
       let maxX = 0;
       let maxY = 0;
-      
+      let minX = Infinity;
+      let minY = Infinity;
+
       this.rues.forEach((rue, rueIndex) => {
         rue.arrets.forEach((arret, arretIndex) => {
           const x = this.calculateAdjustedPosition(rueIndex, arretIndex, 'x');
           const y = this.calculateAdjustedPosition(rueIndex, arretIndex, 'y');
-          
+
           if (x > maxX) maxX = x;
           if (y > maxY) maxY = y;
+          if (x < minX) minX = x;
+          if (y < minY) minY = y;
         });
       });
-      
-      this.viewBox = `0 0 ${maxX + 100} ${maxY + 100}`;
+
+      const padding = 1000; // Adding padding to avoid clipping on the edges
+      this.viewBoxX = minX - padding;
+      this.viewBoxY = minY - padding;
+      this.viewPortWidth = maxX - minX + padding * 2;
+      this.viewPortHeight = maxY - minY + padding * 2;
+      this.updateViewBox();
     },
     showTooltip(event, content) {
       this.tooltip.visible = true;
@@ -178,17 +218,12 @@ export default {
   }
 };
 </script>
-
 <style scoped>
-.container {
-  width: 100vw;
-  height: 100vh;
-  padding: 0;
+html, body, #app {
+  height: 100%;
   margin: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
+  padding: 0;
+  overflow: hidden; /* Prevent page scrolling */
 }
 
 h2 {
@@ -198,16 +233,22 @@ h2 {
   color: #333;
 }
 
-svg {
-  width: 100%;
+.container {
   height: 100%;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 }
 
 .map-container {
+  height: 100%;
   width: 100%;
-  height: calc(100vh - 60px); /* Adjust height based on header */
   position: relative;
-  background: #e5e5e5;
+  background: none; /* Remove background */
 }
 
 .cursor-grab {
